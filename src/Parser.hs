3be1c43accbda925c000e988f04b9e25d7195a91
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2024-04-30 10:02:31 +0300

    added web and lang projects into 'monorepo'

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,13 +1,22 @@
 module Parser() where
 
 import           Control.Applicative
-import           Lexer               (Token)
-import qualified Text.Parsec         as P
+import           Lexer                      (Token)
+import qualified Text.Megaparsec.Byte.Lexer as P
+import qualified Text.Parsec                as P
 
 type Parser = P.Parsec [Token] ()
 
+data Literal = IntLiteral Int | FloatLiteral Double
+    deriving (Show)
 
+data BinaryOp = Plus | Minus | Mult | Div
+    deriving (Show)
 
+data Var = Id String | Liter Literal
+    deriving (Show)
 
+data Expr = LiterExpr Var | BinaryExpr BinaryOp Var Var
+    deriving (Show)
 
-
+-- parseLiteral = P.lexeme
